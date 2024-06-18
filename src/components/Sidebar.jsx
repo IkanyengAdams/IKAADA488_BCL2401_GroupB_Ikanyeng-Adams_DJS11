415c1066ecb4148da3ab5b2eeda2f92fa255bd5d
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-18 10:57:57 +0200

    Added new UI elements to Sidebar component: search icon and 'Your Library' section

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,19 @@ const Sidebar = () => {
         <p className='font-bold'>Home</p>
 
       </div>
+      <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+        <img className='w-6' src={assets.search_icon} alt='' />
+        <p className='font-bold'>Search</p>
+
+      </div>
+      </div>
+      <div className='bg-[#121212] h-[85%] rounded'>
+      <div className='p-4 flex items-center justify-between'>
+      <div className='flex items-center gap-3'>
+      <img className='w-6' src={assets.stack_icon} alt='' />
+      <p className='font-semibold'>Your Library</p>
+      </div>
+      </div>
       </div>
     </div>
   )
